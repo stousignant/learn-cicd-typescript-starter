57cdc3aa9d7615622357274b2b265bfc1b106d46
stousignant <sacha.tousignant@gmail.com>
2026-01-12 08:57:41 -0500

    Lint CI step with fail step

--- src/main.ts
+++ src/main.ts
@@ -11,22 +11,22 @@ import { handlerUsersCreate, handlerUsersGet } from "./api/users.js";
 const __dirname = path.resolve();
 
 if (!config.api.port) {
-  console.error("PORT environment variable is not set");
-  process.exit(1);
+    console.error("PORT environment variable is not set");
+    process.exit(1);
 }
 
 const app = express();
 app.use(express.json());
 
 app.use(
-  cors({
-    origin: ["https://*", "http://*"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: "*",
-    exposedHeaders: ["Link"],
-    credentials: false,
-    maxAge: 300,
-  }),
+    cors({
+        origin: ["https://*", "http://*"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: "*",
+        exposedHeaders: ["Link"],
+        credentials: false,
+        maxAge: 300,
+    }),
 );
 
 app.use("/", express.static(path.join(__dirname, config.api.filepathRoot)));
@@ -34,10 +34,10 @@ app.use("/", express.static(path.join(__dirname, config.api.filepathRoot)));
 const v1Router = express.Router();
 
 if (db) {
-  v1Router.post("/users", handlerUsersCreate);
-  v1Router.get("/users", middlewareAuth(handlerUsersGet));
-  v1Router.get("/notes", middlewareAuth(handlerNotesGet));
-  v1Router.post("/notes", middlewareAuth(handlerNotesCreate));
+    v1Router.post("/users", handlerUsersCreate);
+    v1Router.get("/users", middlewareAuth(handlerUsersGet));
+    v1Router.get("/notes", middlewareAuth(handlerNotesGet));
+    v1Router.post("/notes", middlewareAuth(handlerNotesCreate));
 }
 
 v1Router.get("/healthz", handlerReadiness);
@@ -45,5 +45,10 @@ v1Router.get("/healthz", handlerReadiness);
 app.use("/v1", v1Router);
 
 app.listen(config.api.port, () => {
-  console.log(`Server is running on port: ${config.api.port}`);
+    console.log(`Server is running on port: ${config.api.port}`);
 });
+
+function unused() {
+    // this function does nothing
+    // and is called nowhere
+}
